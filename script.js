a1c2e4dcc0b75aa69924a9c01603d8b7fa9261fb
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-11-11 23:41:13 +0600

    Added Quiz question in js file for quiz

---
+++ script.js
@@ -0,0 +1,29 @@
+const question = [
+  {
+    question: "Which is larger animal in the world.?",
+    answers: [
+      { text: "shark", correct: false },
+      { text: "blue whale", correct: true },
+      { text: "elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is largest desert in the world.?",
+    answers: [
+      { text: "kalahari", correct: false},
+      { text: "Gobi", correct: false },
+      { text: "sahara", correct: false },
+      { text: "Antarctica", correct: true },
+    ]
+  },
+  {
+    question: "Which is smallest continent in the world.?",
+    answers: [
+      { text: "Asia", correct: false },
+      { text: "Australia", correct: true },
+      { text: "Arctic", correct: false },
+      { text: "Africa", correct: false },
+    ],
+  },
+];
