fe5142628041b7fdf18998fa8e9e4a71fdb79035
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-11-12 00:20:38 +0600

    Added function for show question in frontend from js

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ const question = [
     ],
   },
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answe-buttons");
+const nextButton = document.getElementById("next-btn");
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "next";
+  showQuestion();
+}
+
+function showQuestion(){
+  let currentQuestion = question[currentQuestionIndex];
+  let questionNo = currentQuestionIndex +1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}
+
+startQuiz();
